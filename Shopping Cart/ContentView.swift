acc57cb2222004a95f7e778cb02d853a3f28c598
Karan Pal <palkaran@outlook.com>
2025-07-05 15:13:52 +0530

    Initial Commit

---
+++ Shopping Cart/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Shopping Cart
+//
+//  Created by Karan Pal on 05/07/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Shopping Cart/Shopping_CartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_CartApp.swift
+//  Shopping Cart
+//
+//  Created by Karan Pal on 05/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_CartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
